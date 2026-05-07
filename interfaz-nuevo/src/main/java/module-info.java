4074e4b9edaccca344845ee24196ffe97bd4bf28
Álvaro Tarancon <kodek729@outlook.es>
2026-05-07 16:32:24 +0200

    Add files via upload

--- interfaz-nuevo/src/main/java/module-info.java
+++ interfaz-nuevo/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.interfazempresanofraudulenta {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires javafx.swing;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
@@ -11,13 +12,14 @@ module org.example.interfazempresanofraudulenta {
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
-    requires com.itextpdf.kernel;
-    requires com.itextpdf.layout;
-    requires com.itextpdf.io;
-    requires com.itextpdf.commons;
+    // Dependencias de iText 8
+    // Nota: Si usas Maven, estos son los nombres de módulos automáticos correctos
     requires kernel;
     requires layout;
+    requires io;
+    requires commons;
 
+    // Asegúrate de que el paquete ControladorVentana exista en src/main/java
     opens ControladorVentana to javafx.fxml;
     exports ControladorVentana;
 }
